a92565da4953cb910c67d35c52b08465ebafc4ee
nofurtherinformation <dhalpern@uchicago.edu>
2023-04-24 11:07:58 -0500

    Chips fix

--- dextra-components/src/components/Chips/src/Chips.ts
+++ dextra-components/src/components/Chips/src/Chips.ts
@@ -1,5 +1,5 @@
-import { html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 import { OslControl } from "../../Interface/src/Interface";
 import "@spectrum-web-components/tags/sp-tags.js";
 import "@spectrum-web-components/tags/sp-tag.js";
@@ -9,17 +9,16 @@ import "@spectrum-web-components/textfield/sp-textfield.js";
 
 @customElement("osl-chips")
 export class OslChips extends OslControl {
-
-  getJsonValues(){
-    const value = (this.value as Array<string | number>) || [];
-    const options = typeof value === "string" ? JSON.parse(value) : value || [];
-    return options;
-  }
+  @property({ type: Object })
+  override initialValue: Array<string | number> = [];
 
   renderTags() {
-    const options = this.getJsonValues();
+    if (!Array.isArray(this.value) || this.value.length === 0) {
+      return null;
+    }
+
     return html`
-      ${options.map(
+      ${this.value.map(
         (option: string | number) => html`
           <sp-tag
             deleteable
@@ -38,27 +37,59 @@ export class OslChips extends OslControl {
       : html``;
   }
 
-  protected handleRemove(value: string | number | any) {
-    const options = this.getJsonValues();
-    const newValues = options.filter((option: string) => option !== value);
-    this.handleChange({
-      target: {
-        // @ts-ignore
-        value: newValues
-      },
-    });
-    // const options = this.options || [];
-    // console.log(this.store.datasets[this.data].parameters);
-    // const newValues = options.filter((option) => option !== value);
-    // console.log(options, value, newValues);
-    // this.store.datasets[this.data].parameters[this.option] = newValues;
+  protected get inputElement() {
+    return (
+      this.renderRoot.querySelector(`.chips-${this.elementId}`) as LitElement
+    ).renderRoot.querySelector(".input")!;
+  }
+
+  protected get inputValue(): string {
+    // @ts-ignore
+    return this.inputElement.value!;
+  }
+
+  resetText() {
+    // @ts-ignore
+    this.inputElement.value = "";
+  }
+  protected override eventValueAccessor(event: any): string {
+    return event;
+  }
+
+  protected handleRemove(inputValue: string | number | any) {
+    const storeValue = this.value as Array<string | number>;
+    const value = inputValue;
+    const newValues = storeValue.filter((item) => item !== value);
+    // @ts-ignore
+    this.handleChange(newValues);
+  }
+
+  protected handleAdd(event: KeyboardEvent): void {
+    const isEnter = event.key === "Enter";
+    const inputValue = this.inputValue?.trim();
+    const isValidText = inputValue && inputValue.length > 0;
+
+    if (isEnter && isValidText) {
+      const storeValue = this.value as Array<string | number>;
+      const newValues = [...storeValue, inputValue];
+      this.store.datasets[this.data].parameters[this.option] = newValues;
+      // this.value = JSON.stringify(newValues);
+      this.resetText();
+    }
   }
 
   override template() {
     return html`
       <sp-tags> ${this.renderTags()} </sp-tags><br />
-      <sp-field-label for="chips-${this.id}">${this.title}</sp-field-label>
-      <sp-textfield id="chips-${this.id}" placeholder=${this.label as string}>
+      <sp-field-label for="chips-${this.elementId}"
+        >${this.title}</sp-field-label
+      >
+      <sp-textfield
+        class="chips-${this.elementId}"
+        id="chips-${this.elementId}"
+        placeholder=${this.label as string}
+        @keydown=${this.handleAdd}
+      >
         ${this.renderHelpText()}
       </sp-textfield>
     `;
