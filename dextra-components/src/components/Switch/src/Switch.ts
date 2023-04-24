2dec4fd7b53e31edaeb3f02a2147d7e715ced7f2
nofurtherinformation <dhalpern@uchicago.edu>
2023-04-24 14:21:24 -0500

    Switch

--- dextra-components/src/components/Switch/src/Switch.ts
+++ dextra-components/src/components/Switch/src/Switch.ts
@@ -5,10 +5,13 @@ import "@spectrum-web-components/switch/sp-switch.js";
 
 @customElement("osl-switch")
 export class SwitchControl extends OslControl {
+  protected override eventValueAccessor(event: Event): string {
+    return (!(event.target as HTMLInputElement).checked).toString();
+  }
   override template() {
     return html`
-      <sp-switch label="Switch" onclick="spAlert(this, '<sp-switch> clicked!')">
-        Switch
+      <sp-switch label="${this.label}" @click=${this.handleChange}>
+        ${this.title}
       </sp-switch>
     `;
   }
--- dextra-components/src/stories/Switch.stories.ts
+++ dextra-components/src/stories/Switch.stories.ts
@@ -4,10 +4,19 @@ import "../components/Switch/src/Switch.ts";
 
 export default {
   title: "Interface",
-}
+};
 
 export const Switch = () => {
   return html`
-    <osl-switch></osl-switch>
-    `
-}
+  <osl-interface
+    data="https://d2vloi59ojgfpi.cloudfront.net/data-query/6417b73afe20b50008149901"
+    option="mag"
+    ></osl-interface>
+    <br/>
+    <osl-switch
+      data="https://d2vloi59ojgfpi.cloudfront.net/data-query/6417b73afe20b50008149901"
+      option="mag"
+      title="On or Off?"
+    ></osl-switch>
+  `;
+};
