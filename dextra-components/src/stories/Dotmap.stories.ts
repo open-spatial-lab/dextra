90fee0b51999a0d59c8c7f92b43f3d7108e274bd
cindyheqy <cindy.heqy@gmail.com>
2023-07-31 15:58:34 -0500

    add land map

--- dextra-components/src/stories/Dotmap.stories.ts
+++ dextra-components/src/stories/Dotmap.stories.ts
@@ -15,12 +15,22 @@ export const Dotmap = () => {
     projection="equirectangular"
     >
       <osl-choropleth
+      data="http://localhost:6006/data/world.json" 
+      fill="currentColor"
+      fillOpacity="0.2"
+      ></osl-choropleth>
+
+      <osl-choropleth
       data="http://localhost:6006/data/earthquack.json" 
       r="(d) => Math.pow(10, d.properties.mag)"
       fill="red",
       fillOpacity="0.2",
       stroke="red"
+      href="(d) => d.properties.url"
       ></osl-choropleth>
+
     </osl-plot>
     `
 }
+
+
