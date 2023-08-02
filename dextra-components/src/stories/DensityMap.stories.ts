766a65504320a96da28b84093b2586595fdcd357
cindyheqy <cindy.heqy@gmail.com>
2023-08-02 08:59:02 -0500

    create walmart density map

---
+++ dextra-components/src/stories/DensityMap.stories.ts
@@ -0,0 +1,42 @@
+import { Meta } from "@storybook/web-components";
+import { html } from "lit";
+import "../components/marks/Geo/src/Geo.ts";
+import "../components/marks/Density/src/Density.ts";
+import "../components/Marks/Dot/src/Dot.ts";
+import "../components/Plot/src/Plot.ts";
+
+export default {
+  title: "Marks",
+}
+
+export const DensityMap = () => {
+  return html`
+  <osl-plot
+
+    projection="albers" 
+    colorScheme="YlGnBu"
+    colorLegend="true"
+    colorLabel="Walmart Density"
+  >
+    <osl-density-plot
+        data="http://localhost:6006/data/walmarts.tsv" 
+        x="longitude"
+        y="latitude"
+        bandwidth="10"
+        fill="density"
+    ></osl-density-plot>
+  >
+    <osl-geo
+        data="http://localhost:6006/data/nations.json" 
+    ></osl-geo>
+    <osl-dot-plot
+        data="http://localhost:6006/data/walmarts.tsv" 
+        x="longitude"
+        y="latitude"
+        r="1"
+        fill="currentColor"
+    ><osl-dot-plot>
+  </osl-plot>
+
+    `
+}
