fbf5ccd3c2b734a405ffd3589bbb0d0ba36419fd
cindyheqy <cindy.heqy@gmail.com>
2023-08-07 17:27:38 -0500

    Solved same tag name issue with dot plot

--- dextra-components/src/stories/MultiDotMap.stories.ts
+++ dextra-components/src/stories/MultiDotMap.stories.ts
@@ -1,7 +1,6 @@
 import { Meta } from "@storybook/web-components";
 import { html } from "lit";
 import "../components/marks/Geo/src/Geo.ts";
-import "../components/marks/Dot/src/Dot.ts";
 import "../components/Plot/src/Plot.ts";
 import * as d3 from "d3"
 
