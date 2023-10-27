e8ef9db2773952e9b4467dff7de5ccd44ef2dbb4
nofurtherinformation <dhalpern@uchicago.edu>
2023-10-27 10:41:23 -0500

    stories update

--- dextra-components/src/stories/GlMap.stories.ts
+++ dextra-components/src/stories/GlMap.stories.ts
@@ -9,6 +9,9 @@ import "../components/StatTable/src/StatTable.ts";
 import "../components/DataTable/src/DataTable.ts";
 import "../components/Download/src/Download.ts";
 import "../components/Stat/src/Stat.ts";
+import "../components/marks/Line/src/Line.ts";
+import "../components/marks/Axis/src/Axis.ts";
+import "../components/Plot/src/Plot.ts";
 
 export default {
   title: "Data",
@@ -958,6 +961,157 @@ export const ControlledGlMap = () => {
   `;
 };
 
+
+export const Wisconsin = () => {
+  const dates = [
+  
+  '2021-03',
+  '2021-05',
+  '2021-07',
+  '2021-08',
+  '2021-09',
+  '2021-11',
+  '2022-01',
+  '2022-02',
+  '2022-03',
+  '2022-05',
+  '2022-06',
+  '2022-07',
+  '2022-08',
+  '2022-09',
+  '2022-10',
+  '2022-11',
+  '2022-12',
+  '2023-01',
+  '2023-02',
+  '2023-03',
+  '2023-04',
+  '2023-05',
+  '2023-06',
+  '2023-07',
+  '2023-08',
+  '2023-09',]
+  const cccStatDataset = "https://d27ouidsqnp6r.cloudfront.net/data-query/653bc5df7ac484000866bdd4"
+  const lccStatDataset = "https://d27ouidsqnp6r.cloudfront.net/data-query/653bc7487ac484000866bdd5"
+  const tableDataset = "https://d27ouidsqnp6r.cloudfront.net/data-query/653baba66c4cd800085af0b2"
+  const lineDataset = "https://d27ouidsqnp6r.cloudfront.net/data-query/653b1772972e0e0008812306"
+  const allDatasets = JSON.stringify([cccStatDataset, lccStatDataset, tableDataset])
+  const allAndLineDataset = JSON.stringify([cccStatDataset, lccStatDataset, tableDataset, lineDataset])
+  const starOptions = ['*','1','2','3','4','5']
+  const congressionalDistricts = ["*", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+// <sp-help-text>Star Rating</sp-help-text>
+// <osl-select-control
+// data=${allDatasets}
+// options=${JSON.stringify(starOptions)}
+// option="star"
+// initialValue="*"
+// >
+// </osl-select-control>
+  return html`
+  <div style="display:flex;flex-direction:row;gap:1rem;">
+  <div style="display:flex;flex-direction:column">
+  <div>
+  <osl-flex-box breakpoint="md">
+  <sp-help-text>Open On</sp-help-text>
+  <osl-select-control
+data=${allDatasets}
+options=${JSON.stringify(dates)}
+option="open_on"
+initialValue="2021-03"
+>
+</osl-select-control></div>
+<div>
+<sp-help-text>Closed on</sp-help-text>
+  <osl-select-control
+data=${allDatasets}
+options=${JSON.stringify(dates)}
+option="closed_on"
+initialValue="2023-09"
+>
+</osl-select-control>
+<sp-help-text>Congressional District</sp-help-text>
+  <osl-select-control
+data=${allAndLineDataset}
+options=${JSON.stringify(congressionalDistricts)}
+option="congressional_district"
+initialValue="*"
+>
+</osl-select-control>
+
+
+</osl-flex-box>
+</div>
+</div><div style="flex-grow:1;width:100%">
+
+<osl-flex-box
+      breakpoint="sm"
+    >
+
+    <osl-stat
+        title="LCC Capacity Closed"
+        property="LCC Total Capacity Lost"
+        statSize="1rem"
+        data="https://d27ouidsqnp6r.cloudfront.net/data-query/653bc5df7ac484000866bdd4"
+      >
+      </osl-stat>
+      <osl-stat
+        title="Count of LCC Providers Closed"
+        property="LCC Count of Providers Closings"
+        statSize="1rem"
+        data="https://d27ouidsqnp6r.cloudfront.net/data-query/653bc5df7ac484000866bdd4"
+      >
+      </osl-stat>
+    <osl-stat
+        title="CCC Capacity Closed"
+        property="CCC Total Capacity Lost"
+        statSize="1rem"
+        data="https://d27ouidsqnp6r.cloudfront.net/data-query/653bc7487ac484000866bdd5"
+      >
+      </osl-stat>
+      <osl-stat
+        title="Count of CCC Providers Closed"
+        property="CCC Count of Providers Closings"
+        statSize="1rem"
+        data="https://d27ouidsqnp6r.cloudfront.net/data-query/653bc7487ac484000866bdd5"
+      >
+      </osl-stat>
+    </osl-flex-box>
+<div style="height: 60vh; width: 100%;">
+
+<osl-plot
+colorLegend="true" 
+data="https://d27ouidsqnp6r.cloudfront.net/data-query/653b1772972e0e0008812306"    
+>
+<osl-line 
+x="Date"
+y="total_capacity"
+direction="horizontal"
+></osl-line>
+</osl-plot>
+</div>
+<hr/>
+  <div style="height:80vh; width:100%">
+  <osl-flex-box breakpoint="md">
+  <h3>Closed during this period</h3>
+
+  <osl-download
+      title="Download Providers Data"
+      filename="closed-summary"
+      tsv
+      parameterSuffixes=${JSON.stringify(["open_on", "closed_on"])}
+      data="https://d27ouidsqnp6r.cloudfront.net/data-query/653baba66c4cd800085af0b2"
+    ></osl-download>
+  </osl-flex-box>
+  <osl-data-table
+      data="https://d27ouidsqnp6r.cloudfront.net/data-query/653baba66c4cd800085af0b2"
+      columns='["Provider Number","ADDRESS", "CITY", "ZIP", "Capacity", "Application Type", "Star Level ", "CONGRESSIONAL_DISTRICT", "ASM2021", "SENATE_DISTRICT", "GEOID"]'
+      pagination="true"
+    ></osl-data-table>
+    
+  <div>
+</div></div>`
+}
 
 export const m3 = () => {
   return html`
--- dextra-components/src/stories/Line.stories.ts
+++ dextra-components/src/stories/Line.stories.ts
@@ -3,20 +3,33 @@ import { html } from "lit";
 import "../components/marks/Line/src/Line.ts";
 import "../components/marks/Axis/src/Axis.ts";
 import "../components/Plot/src/Plot.ts";
+import "../components/Select/src/Select.ts";
+import "../components/Select/src/SelectControl.ts";
 
 export default {
   title: "Marks",
 };
 export const TestLine = () => {
   return html`
+  <osl-select-control
+data="https://d27ouidsqnp6r.cloudfront.net/data-query/653b1ac2972e0e000881230a"    
+options='["1","2","3","4","5","6","7","8"]'
+option="congressional_district"
+defaultOption="1"
+initialValue="1"
+>
+</osl-select-control>
+<br/>&nbsp;
   <div style="height: 60vh; width: 100%">
-  <osl-plot
+
+<osl-plot
 colorLegend="true" 
 data="https://d27ouidsqnp6r.cloudfront.net/data-query/653b1ac2972e0e000881230a"    
 >
 <osl-line 
 x="Date"
 y="total_capacity"
+direction="horizontal"
 ></osl-line>
 </osl-plot>
 </div>
