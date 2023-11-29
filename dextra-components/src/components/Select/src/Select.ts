eb145d59731f7c1c1008c8092d7773b863d679db
nofurtherinformation <dhalpern@uchicago.edu>
2023-11-29 12:59:50 -0600

    Virtualization in select list

--- dextra-components/src/components/Select/src/Select.ts
+++ dextra-components/src/components/Select/src/Select.ts
@@ -9,7 +9,8 @@ import "@spectrum-web-components/overlay/overlay-trigger.js";
 import "@spectrum-web-components/tooltip/sp-tooltip.js";
 import "@spectrum-web-components/popover/sp-popover.js";
 import "@spectrum-web-components/dialog/sp-dialog.js";
-import '@spectrum-web-components/icons-ui/icons/sp-icon-chevron100.js';
+import "@spectrum-web-components/icons-ui/icons/sp-icon-chevron100.js";
+import "@lit-labs/virtualizer";
 
 import { OslControl } from "../../Interface/src/Interface";
 import { query } from "lit-element";
@@ -32,23 +33,55 @@ export class Select extends OslControl {
     toggleButton.click();
   }
 
-  renderButton() {
-    const valueLabel = this.getValueLabel();
-    return html`
-      <sp-action-button slot="trigger" id="toggle" style="max-width:100%;">
-        ${valueLabel}
-        <sp-icon-chevron100 size="s" style="margin-left:0.5rem;transform:scale(0.75) rotate(90deg);"></sp-icon-chevron100>
-      </sp-action-button>
-    `;
-  }
-
   get buttonDimensions() {
     const dims = this.toggleButton?.getBoundingClientRect();
     if (!dims) return { width: 0, height: 0 };
     return dims;
   }
 
-  handleChange(event: Event){
+  static styles = css`
+    #virtualized-list {
+      min-width: 400px;
+      width: fit-content;
+      max-width: 100vw;
+      min-height: 400px;
+      height: fit-content;
+      max-height: 100vh;
+    }
+    :host sp-menu-item {
+      background: red !important;
+    }
+  `;
+
+  forceVirtualListInitialize() {
+    const virtualizer = this.shadowRoot?.querySelector("#virtualized-list");
+    try {
+      // @ts-ignore
+      virtualizer.element(1).scrollIntoView({
+        block: "top",
+      });
+    } catch (e) {}
+  }
+
+  renderButton() {
+    const valueLabel = this.getValueLabel();
+    return html`
+      <sp-action-button
+        slot="trigger"
+        id="toggle"
+        style="max-width:100%;"
+        @click=${this.forceVirtualListInitialize}
+      >
+        ${valueLabel}
+        <sp-icon-chevron100
+          size="s"
+          style="margin-left:0.5rem;transform:scale(0.75) rotate(90deg);"
+        ></sp-icon-chevron100>
+      </sp-action-button>
+    `;
+  }
+
+  handleChange(event: Event) {
     super.handleChange(event);
     if (!this.multipleSelect) {
       this.toggleOpen();
@@ -58,10 +91,8 @@ export class Select extends OslControl {
     const value = this.value || "";
     return html`
       <sp-popover slot="click-content" direction="bottom-start">
-        <sp-menu
-          @click=${this.handleChange}
-        > 
-          ${this.renderDropdownOptions(value)} 
+        <sp-menu @click=${this.handleChange}>
+          ${this.renderDropdownOptions(value)}
         </sp-menu>
       </sp-popover>
     `;
@@ -83,14 +114,19 @@ export class Select extends OslControl {
     const getValue = (v: string | number) => {
       return Array.isArray(value) ? value.includes(v) : value === v;
     };
-    return html`${options.map((option) => {
-      const label = this.getOptionText(option);
-      const value = this.getOptionValue(option);
-      const isSelected = getValue(value);
-      return isSelected
-        ? this.renderSelectedOption(label, value)
-        : this.renderUnselectedOption(label, value);
-    })}`;
+    return html` <lit-virtualizer
+      id="virtualized-list"
+      scroller
+      .items=${options}
+      .renderItem=${(option: any) => {
+        const label = this.getOptionText(option);
+        const value = this.getOptionValue(option);
+        const isSelected = getValue(value);
+        return isSelected
+          ? this.renderSelectedOption(label, value)
+          : this.renderUnselectedOption(label, value);
+      }}
+    ></lit-virtualizer>`;
   }
 
   protected renderTitle() {
